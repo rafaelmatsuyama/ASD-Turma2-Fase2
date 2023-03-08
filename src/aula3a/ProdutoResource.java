b0ca2d1d23804b25f447d1ed1d07a2c9e9d501c2
Rafael Matsuyama <rafael.matsuyama@gmail.com>
2023-03-07 21:31:11 -0300

    Adicionado o Lab envolvendo o uso de Spring MVC a partir do lab da última aula.

---
+++ src/aula3a/ProdutoResource.java
@@ -0,0 +1,23 @@
+package aula3a;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("estoque")
+public class ProdutoResource {
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping("saudacao")
+    public String saudacao(@RequestParam(name="nome", required=false, defaultValue="Fulano") String name, Model model) {
+        model.addAttribute("nome", name);
+        return "greeting";
+    }
+}
